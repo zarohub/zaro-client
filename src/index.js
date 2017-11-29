d0afa4d5e94491e735a547abebc2fabae17d4f7c
Martin Adams <martin@meda.co.uk>
2017-11-29 17:12:38 +0000

    Refactor to use async/await

--- src/index.js
+++ src/index.js
@@ -1,278 +1,222 @@
-import ZenttyClient from 'zentty-client';
+//import ZenttyClient from 'zentty-client';
+import ZenttyClient from '../../../zentty/zentty-client/src';
 import gql from 'graphql-tag';
 
 class ZaroClient extends ZenttyClient {
     
-    /**
-     * params {
-     *   teamID: String!
-     * }
-     */
-    getTeamMembers (params) {
-        return new Promise((fulfill, reject) => {
-            this.client.query({
-                fetchPolicy: 'network-only',
-                query: gql`
-                    query getTeamMembers($teamID: String!, $offset: Int = 0, $limit: Int = 100) {
-                    	getTeamMembers(
-                    	    teamID: $teamID,
-                    	    offset: $offset,
-                    	    limit: $limit
-                    	) {
-                    	    result {
-                    	        total
-                    	        limit
-                    	        offset
-                    	    }
-                    	    items {
-                    	        user {
-                        		    _id
-                        		    username
-                        		    email
-                        		    name
-                        		    active
-                        		    registered
-                        		    avatarUrl
-                    	        }
-                    	        dateJoined
-                    	        role
-                    	        status
-                    	    }
-                    	}
-                    }
-                `,
-                variables: {
-                    ...params
-                }
-            }).then(result => {
-                fulfill(result.data.getTeamMembers);
-            }).catch(err => reject(err));
-        });
-    }
-    
-    /**
-     * params {
-     *   limit: Int
-     *   offset: Int
-     *   archived: Booelan
-     * }
-     */
-    getTeams (params = {}) {
-        return new Promise((fulfill, reject) => {
-            this.client.query({
-                fetchPolicy: 'network-only',
-                query: gql`
-                    query getTeams($offset: Int = 0, $limit: Int = 100, $archived: Boolean = false) {
-                    	getTeams(
-                    	    offset: $offset,
-                    	    limit: $limit,
-                    	    archived: $archived
-                    	) {
-                    	    result {
-                    	        total
-                    	        limit
-                    	        offset
-                    	    }
-                    	    items {
-                                _id
+    async getTeamMembers (params) {
+        const { data: { getTeamMembers: teamMembers }} = await this.client.query({
+            fetchPolicy: 'network-only',
+            query: gql`
+                query getTeamMembers($teamID: String!, $offset: Int = 0, $limit: Int = 100) {
+                	getTeamMembers(
+                	    teamID: $teamID,
+                	    offset: $offset,
+                	    limit: $limit
+                	) {
+                	    result {
+                	        total
+                	        limit
+                	        offset
+                	    }
+                	    items {
+                	        user {
+                    		    _id
+                    		    username
                     		    name
-                    		    subscription {
-                    		        plan {
-                    		            _id
-                    		            name
-                    		            benefits {
-                    		                title
-                    		                description
-                    		                included
-                    		            }
-                    		            monthlyPrice
-                    		        }
-                    		        type
-                    		        status
-                    		    }
-                    		    archived
-                    		    createdAt
-                    		    teamMembers {
-                    		        user {
-                    		            _id
-                    		            username
-                    		            name
-                    		            active
-                    		            registered
-                    		            avatarUrl
-                    		        }
-                    		        dateJoined
-                    		        role
-                    		        status
-                    		    }
-                    		    teamMemberCount
-                    		    projectCount
-                    	    }
-                    	}
-                    }
-                `,
-                variables: {
-                    ...params
+                    		    active
+                    		    registered
+                    		    avatarUrl
+                	        }
+                	        dateJoined
+                	        role
+                	        status
+                	    }
+                	}
                 }
-            }).then(result => {
-                fulfill(result.data.getTeams);
-            }).catch(err => reject(err));
+            `,
+            variables: {
+                ...params
+            }
         });
+        return teamMembers;
     }
     
-    /**
-     * params {
-     *  id: String!
-     * }
-     */
-    getTeam (params) {
-        return new Promise((fulfill, reject) => {
-            this.client.query({
-                fetchPolicy: 'network-only',
-                query: gql`
-                    query getTeam($id: String!) {
-                    	getTeam(
-                            id: $id
-                    	) {
+    async getTeams (params = {}) {
+        const { data: { getTeams: teams }} = await this.client.query({
+            fetchPolicy: 'network-only',
+            query: gql`
+                query getTeams($offset: Int = 0, $limit: Int = 100, $archived: Boolean = false) {
+                	getTeams(
+                	    offset: $offset,
+                	    limit: $limit,
+                	    archived: $archived
+                	) {
+                	    result {
+                	        total
+                	        limit
+                	        offset
+                	    }
+                	    items {
                             _id
                 		    name
                 		    subscription {
                 		        plan {
                 		            _id
                 		            name
-                		            benefits {
-                		                title
-                		                description
-                		                included
-                		            }
-                		            monthlyPrice
                 		        }
                 		        type
                 		        status
                 		    }
                 		    archived
                 		    createdAt
-                		    teamMembers {
-                		        user {
-                		            _id
-                		            username
-                		            name
-                		            active
-                		            registered
-                		            avatarUrl
+                		    getMembers(limit: 10) {
+                		        result {
+                		            total
                 		        }
-                		        dateJoined
-                		        role
-                		        status
+                		        items {
+                		            user {
+                		                _id
+                		                username
+                		                name
+                		                active
+                		                registered
+                		                avatarUrl
+                		            }
+                		            dateJoined
+                		            role
+                		            status
+                		        }
                 		    }
-                		    teamMemberCount
                 		    projectCount
-                    	}
-                    }
-                `,
-                variables: {
-                    ...params
+                	    }
+                	}
                 }
-            }).then(result => {
-                fulfill(result.data.getTeam);
-            }).catch(err => reject(err));
+            `,
+            variables: {
+                ...params
+            }
         });
+        
+        return teams;
     }
     
-    /**
-     * params {
-     *  name: String!
-     * }
-     */
-    createTeam (params) {
-        return new Promise((fulfill, reject) => {
-            this.client.mutate({
-                mutation: gql`
-                    mutation createTeam($name: String!) {
-                    	createTeam(
-                    	    name: $name
-                    	) {
-                    		team {
-                    		    _id
-                    		    name
-                    		    subscription {
-                    		        plan {
-                    		            _id
-                    		            name
-                    		            benefits {
-                    		                title
-                    		                description
-                    		                included
-                    		            }
-                    		            monthlyPrice
-                    		        }
-                    		        type
-                    		        status
-                    		    }
-                    		    archived
-                    		    createdAt
-                    		    teamMembers {
-                    		        user {
-                    		            _id
-                    		            username
-                    		            name
-                    		            active
-                    		            registered
-                    		            avatarUrl
-                    		        }
-                    		        dateJoined
-                    		        role
-                    		        status
-                    		    }
-                    		    teamMemberCount
-                    		    projectCount
-                    		}
-                		    errors {
-                		        field
-                		        message
-                		    }
-                    	}
-                    }
-                `,
-                variables: {
-                    ...params
+    async getTeam (params) {
+        const { data: { getTeam: team }} = await this.client.query({
+            fetchPolicy: 'network-only',
+            query: gql`
+                query getTeam($id: String!) {
+                	getTeam(
+                        id: $id
+                	) {
+                        _id
+            		    name
+            		    subscription {
+            		        plan {
+            		            _id
+            		            name
+            		        }
+            		        type
+            		        status
+            		    }
+            		    archived
+            		    createdAt
+            		    getMembers(limit: 10) {
+            		        result {
+            		            total
+            		        }
+            		        items {
+            		            user {
+            		                _id
+            		                username
+            		                name
+            		                active
+            		                registered
+            		                avatarUrl
+            		            }
+            		            dateJoined
+            		            role
+            		            status
+            		        }
+            		    }
+            		    projectCount
+                	}
                 }
-            }).then(result => {
-                if (result.data.createTeam.errors !== null) {
-                    reject(result.data.createTeam.errors);
-                } else {
-                    fulfill(result.data.createTeam.team);
-                }
-            }).catch(err => reject(err));
+            `,
+            variables: {
+                ...params
+            }
         });
+        
+        return team;
     }
     
-    /**
-     * params {
-     *  teamID: String!
-     *  userIdentifier: String!
-     *  role: String!
-     * }
-     */
-    inviteTeamMember (params) {
-        return new Promise((fulfill, reject) => {
-            this.client.mutate({
-                mutation: gql`
-                    mutation inviteTeamMember($teamID: String!, $userIdentifier: String!, $role: String!) {
-                    	inviteTeamMember(
-                    	    teamID: $teamID,
-                    	    userIdentifier: $userIdentifier,
-                    	    role: $role
-                    	)
-                    }
-                `,
-                variables: {
-                    ...params
+    async createTeam (params) {
+        const { data: { createTeam: team }} = await this.client.mutate({
+            mutation: gql`
+                mutation createTeam($name: String!) {
+                	createTeam(
+                	    name: $name
+                	) {
+                		_id
+            		    name
+            		    subscription {
+            		        plan {
+            		            _id
+            		            name
+            		        }
+            		        type
+            		        status
+            		    }
+            		    archived
+            		    createdAt
+            		    getMembers(limit: 10) {
+            		        result {
+            		            total
+            		        }
+            		        items {
+            		            user {
+            		                _id
+            		                username
+            		                name
+            		                active
+            		                registered
+            		                avatarUrl
+            		            }
+            		            dateJoined
+            		            role
+            		            status
+            		        }
+            		    }
+            		    projectCount
+                	}
                 }
-            }).then(result => {
-                fulfill(result.data.inviteTeamMember);
-            }).catch(err => reject(err));
+            `,
+            variables: {
+                ...params
+            }
         });
+        
+        return team;
+    }
+    
+    async inviteTeamMember (params) {
+        const { data: { inviteTeamMember: success }} = await this.client.mutate({
+            mutation: gql`
+                mutation inviteTeamMember($teamID: String!, $userIdentifier: String!, $role: String!) {
+                	inviteTeamMember(
+                	    teamID: $teamID,
+                	    userIdentifier: $userIdentifier,
+                	    role: $role
+                	)
+                }
+            `,
+            variables: {
+                ...params
+            }
+        });
+        
+        return success;
     }
 }
 
